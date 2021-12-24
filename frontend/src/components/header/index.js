d461f78dcd079d27a0b9b0d5970b15e5d7dea517
test <kxxo.ea@gmail.com>
2021-12-24 11:24:45 +0300

    new domain

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -3,7 +3,7 @@ function Header() {
     return (
         <header className="u-clearfix u-header u-header">
             <div className="u-clearfix u-sheet u-sheet-1">
-                <p className="u-text u-text-default u-text-1">QR Code For Auto</p>
+                <p className="u-text u-text-default u-text-1">{process.env.REACT_APP_PROJECT_NAME}</p>
             </div>
         </header>
     )
